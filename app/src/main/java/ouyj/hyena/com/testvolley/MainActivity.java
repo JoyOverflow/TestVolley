960faac9adb32b028ecb60c3ebc86905a7fba252
joyoverflow <joyoverflow@sina.com>
2019-10-31 21:25:44 +0800

    修改获取Json数据对象

--- app/src/main/java/ouyj/hyena/com/testvolley/MainActivity.java
+++ app/src/main/java/ouyj/hyena/com/testvolley/MainActivity.java
@@ -3,8 +3,12 @@ package ouyj.hyena.com.testvolley;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,32 +23,77 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.util.List;
+
 import ouyj.hyena.com.testvolley.model.Result;
 import ouyj.hyena.com.testvolley.model.Weathers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    RequestQueue queue;
+    TextView txtMessage;
+    ProgressBar progressBar;
+    ImageView showImg;
+    NetworkImageView networkImg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //初始化请求队列
+        queue = Volley.newRequestQueue(this);
+
+        //查找视图
+        txtMessage = findViewById(R.id.txt_message);
+        progressBar = findViewById(R.id.progressBar);
+        showImg =  findViewById(R.id.showImage);
+        networkImg = findViewById(R.id.img_network);
+
+        //设置按钮事件
+        findViewById(R.id.string_request).setOnClickListener(this);
+        findViewById(R.id.json_request).setOnClickListener(this);
+        findViewById(R.id.image_request).setOnClickListener(this);
+        findViewById(R.id.image_loader).setOnClickListener(this);
+        findViewById(R.id.post).setOnClickListener(this);
+
         //getWebString();
         //getWebObject();
         //getNetImageView();
     }
-
-
-
-
-
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.string_request:
+                getWebString();
+                break;
+            case R.id.json_request:
+                getWebObject();
+                break;
+            case R.id.image_request:
+                //iamgeRequest();
+                break;
+            case R.id.image_loader:
+                //imageLoader();
+                break;
+            case R.id.post:
+                //postRequest();
+                break;
+            default:
+                break;
+        }
+    }
 
     /**
      * 获取Json字串数据
      */
     private void getWebString() {
+        txtMessage.setText("");
+        progressBar.setVisibility(View.VISIBLE);
+
+        //发送Get请求
         String url = "http://api.k780.com/?app=weather.history&weaid=1&date=2019-10-30&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json";
         StringRequest request = new StringRequest(
                 Request.Method.GET,
@@ -54,6 +103,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onResponse(String response) {
                         Log.d(TAG, "onResponse：" + response);
+
+                        //文本显示视图设置文本
+                        txtMessage.setText(response);
+                        progressBar.setVisibility(View.GONE);
                     }
                 },
                 new Response.ErrorListener() {
@@ -61,19 +114,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d(TAG, "onErrorResponse：" + error.getMessage());
+                        progressBar.setVisibility(View.GONE);
                     }
                 }
         );
         //加入到请求队列中
         request.setTag("stringRequest");
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         queue.add(request);
     }
-
     /**
      * 从返回的Json串获取到Java对象
      */
     private void getWebObject() {
+        txtMessage.setText("");
+        progressBar.setVisibility(View.VISIBLE);
+        String url = "http://api.k780.com/?app=weather.history&weaid=1&date=2019-10-30&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                (String)null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        //将Json字串反序列为指定对象
+                        StringBuilder sb = new StringBuilder();
+                        Weathers weatherList = new Gson().fromJson(response.toString(), Weathers.class);
+
+                        if(weatherList != null) {
+                            //显示对象信息（若无此键会返回null，值为数组）
+                            List<Result> results=weatherList.getResult();
+                            if(results!=null) {
+                                for (Result r : weatherList.getResult()) {
+                                    String result = String.format("城市：%s，天气：%s<br/>", r.getCitynm(), r.getWeather());
+                                    sb.append(result);
+                                    Log.d(TAG, result);
+                                }
+                            }
+                        }
+                        //使字串在文本显示视图中换行
+                        txtMessage.append(Html.fromHtml(sb.toString()));
+                        progressBar.setVisibility(View.GONE);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onErrorResponse：" + error.getMessage());
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+        );
+        queue.add(jsonObjectRequest);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * 从返回的Json串获取到Java对象
+     */
+    private void getWebObject2() {
 
         String url = "http://api.k780.com/?app=weather.history&weaid=1&date=2019-10-30&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
@@ -184,8 +299,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imgView.setImageUrl(url,imgLoader);
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }
